a8671768154755910163c335eb38e34c0b3adae5
GlennOu66304 <2738328919@qq.com>
2020-11-26 14:12:14 +0800

    Card Finish

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -15,7 +15,38 @@ function Cards() {
             text='Explore the hidden waterfall deep inside the Amazon Jungle'
             label='Adventure'
             path='/services'
-           />           
+           />   
+
+           <CardItem 
+            src='images2/img-2.jpg'
+            text='Travel through the island of Bali in a Private Cruise'
+            label='Luxury'
+            path='/services'
+           />         
+          </ul>
+
+          <ul className='cards_items'>
+           <CardItem 
+            src='images2/img-3.jpg'
+            text='Set Sail in the Atlantic Ocean visiting'
+            label='Mystery'
+            path='/services'
+           />   
+
+           <CardItem 
+            src='images2/img-4.jpg'
+            text='Experience Football on top of the Himilayan'
+            label='Adventure'
+            path='/products'
+           />   
+
+           <CardItem 
+            src='images2/img-8.jpg'
+            text='Ride through the Sahara Desert on a guide'
+            label='Adrenaline'
+            path='/sign-up'
+           />   
+
           </ul>
          </div>
         </div>
